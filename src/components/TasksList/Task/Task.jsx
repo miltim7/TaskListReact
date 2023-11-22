c18be776d2401e9edda04ebffc288ac13b408d15
miltim7 <timursailing70@gmail.com>
2023-11-22 17:41:34 +0400

    Add Edit/View

--- src/components/TasksList/Task/Task.jsx
+++ src/components/TasksList/Task/Task.jsx
@@ -1,8 +1,11 @@
 import { useDispatch } from "react-redux";
 import { deleteTask, updateTask } from "../../../redux/slices/tasksSlice";
 import { useState } from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import EditTask from "../../EditTask";
+import ViewTask from '../../ViewTask';
 
-function Task({taskData}) {
+function Task({ taskData }) {
     const dispatch = useDispatch();
     const [isChecked, setChecked] = useState(taskData.isChecked);
 
@@ -16,13 +19,35 @@ function Task({taskData}) {
         }));
     }
 
+    const [isEdit, setEdit] = useState(false);
+    const [isView, setView] = useState(false);
+
+    function handleEditClick(e) {
+        e.preventDefault();
+        window.location.href = `/tasks/${taskData.id}/edit`;
+        setEdit(true);
+    }
+
+    function handleViewClick(e) {
+        e.preventDefault();
+        window.location.href = `/tasks/${taskData.id}/view`;
+        setView(true);
+    }
+
     return (
-        <div>
-            <i>{taskData.title}</i>
-            <button>Edit</button>
+        <>
+            <i onClick={handleViewClick}>{taskData.title}</i>
+            <button onClick={handleEditClick} type="submit">Edit</button>
             <button onClick={() => dispatch(deleteTask(taskData.id))}>Delete</button>
-            <input type="checkbox" checked={isChecked} onClick={handleClickChecker}/>
-        </div>
+            <input type="checkbox" checked={isChecked} onClick={handleClickChecker} />
+
+            {
+                isEdit ? <EditTask /> : <></>
+            }
+            {
+                isView ? <ViewTask /> : <></>
+            }
+        </>
     )
 }
 
